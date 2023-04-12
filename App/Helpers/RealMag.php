e6996deca40a2d59f00c95426aae7e8093dadeaa
Alagesan <alagesanbe08@gmail.com>
2023-04-12 17:35:39 +0530

    - Fix: coupon amount and myaccount display

--- App/Helpers/RealMag.php
+++ App/Helpers/RealMag.php
@@ -9,83 +9,93 @@ namespace Wlac\App\Helpers;
 
 use Wlr\App\Helpers\Woocommerce;
 
-class RealMag implements Currency {
-	public static $instance = null;
+class RealMag implements Currency
+{
+    public static $instance = null;
 
-	function getDefaultProductPrice( $product_price, $product, $item, $is_redeem, $order_currency ) {
-		return $product_price;
-	}
+    function getDefaultProductPrice($product_price, $product, $item, $is_redeem, $order_currency)
+    {
+        return $product_price;
+    }
 
-	function getProductPrice( $product_price, $item, $is_redeem, $order_currency ) {
-		if ( empty( $order_currency ) ) {
-			$order_currency = $this->getCurrentCurrencyCode( $order_currency );
-		}
-		$default_currency = $this->getDefaultCurrency();
-		if ( $order_currency == $default_currency ) {
-			return $product_price;
-		}
-		if ( ! empty( $order_currency ) ) {
-			return $this->convertToDefaultCurrency( $product_price, $order_currency );
-		}
-		return $product_price;
-	}
+    function getProductPrice($product_price, $item, $is_redeem, $order_currency)
+    {
+        if (empty($order_currency)) {
+            $order_currency = $this->getCurrentCurrencyCode($order_currency);
+        }
+        $default_currency = $this->getDefaultCurrency();
+        if ($order_currency == $default_currency) {
+            return $product_price;
+        }
+        if (!empty($order_currency)) {
+            return $this->convertToDefaultCurrency($product_price, $order_currency);
+        }
+        return $product_price;
+    }
 
-	function getCurrentCurrencyCode( $code = '' ) {
-		global $WOOCS;
-		return isset( $WOOCS->current_currency ) ? $WOOCS->current_currency : $code;
-	}
+    function getCurrentCurrencyCode($code = '')
+    {
+        global $WOOCS;
+        return isset($WOOCS->current_currency) ? $WOOCS->current_currency : $code;
+    }
 
-	function getDefaultCurrency( $code = '' ) {
-		global $WOOCS;
-		return $WOOCS->default_currency;
-	}
+    function getDefaultCurrency($code = '')
+    {
+        global $WOOCS;
+        return $WOOCS->default_currency;
+    }
 
-	function convertToDefaultCurrency( $amount, $current_currency_code ) {
-		$default_currency = $this->getDefaultCurrency();
-		if ( ! empty( $default_currency ) && $default_currency == $current_currency_code ) {
-			return $amount;
-		}
-		global $WOOCS;
-		$currencies = $WOOCS->get_currencies();
-		$rate       = isset( $currencies[ $current_currency_code ]['rate'] ) && ! empty( $currencies[ $current_currency_code ]['rate'] ) ? $currencies[ $current_currency_code ]['rate'] : 0;
-		$decimal    = isset( $currencies[ $current_currency_code ]['decimals'] ) && ! empty( $currencies[ $current_currency_code ]['decimals'] ) ? $currencies[ $current_currency_code ]['decimals'] : 2;
-		if ( $rate > 0 ) {
-			$amount = $WOOCS->back_convert( $amount, $rate, $decimal );
-		}
-		return (float) $amount;
-	}
+    function convertToDefaultCurrency($amount, $current_currency_code)
+    {
+        $default_currency = $this->getDefaultCurrency();
+        if (!empty($default_currency) && $default_currency == $current_currency_code) {
+            return $amount;
+        }
+        global $WOOCS;
+        $currencies = $WOOCS->get_currencies();
+        $rate = isset($currencies[$current_currency_code]['rate']) && !empty($currencies[$current_currency_code]['rate']) ? $currencies[$current_currency_code]['rate'] : 0;
+        $decimal = isset($currencies[$current_currency_code]['decimals']) && !empty($currencies[$current_currency_code]['decimals']) ? $currencies[$current_currency_code]['decimals'] : 2;
+        if ($rate > 0) {
+            $amount = $WOOCS->back_convert($amount, $rate, $decimal);
+        }
+        return (float)$amount;
+    }
 
-	function convertOrderTotal( $total, $order ) {
-		$woocommerce_helper = Woocommerce::getInstance();
-		$order_data         = $woocommerce_helper->isMethodExists( $order, 'get_data' ) ? $order->get_data() : '';
-		$order_currency     = ! empty( $order_data ) && is_array( $order_data ) && isset( $order_data['currency'] ) && ! empty( $order_data['currency'] ) ? $order_data['currency'] : '';
-		$default_currency   = $this->getDefaultCurrency();
-		if ( $order_currency != $default_currency ) {
-			$total = $this->convertToDefaultCurrency( $total, $order_currency );
-		}
-		return $total;
-	}
+    function convertOrderTotal($total, $order)
+    {
+        $woocommerce_helper = Woocommerce::getInstance();
+        $order_data = $woocommerce_helper->isMethodExists($order, 'get_data') ? $order->get_data() : '';
+        $order_currency = !empty($order_data) && is_array($order_data) && isset($order_data['currency']) && !empty($order_data['currency']) ? $order_data['currency'] : '';
+        $default_currency = $this->getDefaultCurrency();
+        if ($order_currency != $default_currency) {
+            $total = $this->convertToDefaultCurrency($total, $order_currency);
+        }
+        return $total;
+    }
 
-	public static function getInstance( array $config = array() ) {
-		if ( ! self::$instance ) {
-			self::$instance = new self( $config );
-		}
-		return self::$instance;
-	}
+    public static function getInstance(array $config = array())
+    {
+        if (!self::$instance) {
+            self::$instance = new self($config);
+        }
+        return self::$instance;
+    }
 
-	function getCartSubtotal( $sub_total, $cart_data ) {
-		$current_currency = $this->getCurrentCurrencyCode();
-		return $this->convertToDefaultCurrency( $sub_total, $current_currency );
-	}
+    function getCartSubtotal($sub_total, $cart_data)
+    {
+        $current_currency = $this->getCurrentCurrencyCode();
+        return $this->convertToDefaultCurrency($sub_total, $current_currency);
+    }
 
-	function getOrderSubtotal( $sub_total, $order_data ) {
-		$woocommerce_helper = Woocommerce::getInstance();
-		$order              = $woocommerce_helper->getOrder( $order_data );
-		$order_data         = $woocommerce_helper->isMethodExists( $order, 'get_data' ) ? $order->get_data() : '';
-		$order_currency     = ! empty( $order_data ) && is_array( $order_data ) && isset( $order_data['currency'] ) && ! empty( $order_data['currency'] ) ? $order_data['currency'] : '';
-		if ( ! empty( $order_currency ) ) {
-			return $this->convertToDefaultCurrency( $sub_total, $order_currency );
-		}
-		return $sub_total;
-	}
+    function getOrderSubtotal($sub_total, $order_data)
+    {
+        $woocommerce_helper = Woocommerce::getInstance();
+        $order = $woocommerce_helper->getOrder($order_data);
+        $order_data = $woocommerce_helper->isMethodExists($order, 'get_data') ? $order->get_data() : '';
+        $order_currency = !empty($order_data) && is_array($order_data) && isset($order_data['currency']) && !empty($order_data['currency']) ? $order_data['currency'] : '';
+        if (!empty($order_currency)) {
+            return $this->convertToDefaultCurrency($sub_total, $order_currency);
+        }
+        return $sub_total;
+    }
 }
--- App/Router.php
+++ App/Router.php
@@ -9,35 +9,39 @@ namespace Wlac\App;
 
 use Wlac\App\Controllers\Site\Main;
 
-defined( 'ABSPATH' ) or die;
+defined('ABSPATH') or die;
 
-class Router {
-	private static $site, $admin;
+class Router
+{
+    private static $site, $admin;
 
-	function init() {
-		self::$site = empty( self::$site ) ? new Main() : self::$site;
-		//self::$admin = empty(self::$admin) ? new Main() : self::$admin;
-		add_filter( 'wlr_core_multicurrency_allowed', function ( $status ) {
-			return false;
-		} );
-		if ( is_admin() ) {
-			add_action( 'admin_menu', array( self::$site, 'addMenu' ) );
-			add_action( 'admin_footer', array( self::$site, 'menuHide' ) );
-		}
-		add_filter( 'wlr_default_product_price', array( self::$site, 'getDefaultProductPrice' ), 10, 5 );
-		add_filter( 'wlr_product_price', array( self::$site, 'getProductPrice' ), 10, 4 );
-		add_filter( 'wlr_convert_to_default_currency', array( self::$site, 'convertToDefaultCurrency' ), 10, 2 );
-		/*Woocommerce Helper Functions*/
-		add_filter( 'wlr_current_currency', array( self::$site, 'getCurrentCurrencyCode' ) );
-		//Purchase Last order amount
-		add_filter( 'wlr_get_order_total', array( self::$site, 'handleWoocommerceHelperOrderTotal' ), 10, 2 );
-		/* Conditions*/
-		/*Subtotal Conditions*/
-		add_filter( 'wlr_get_cart_subtotal', array( self::$site, 'getCartSubtotal' ), 10, 2 );
-		add_filter( 'wlr_get_order_subtotal', array( self::$site, 'getOrderSubtotal' ), 10, 2 );
-		/*Life time sale value*/
-		//add_filter( 'wlr_life_time_sale_value_order_total', array( self::$site, 'handleConditionOrderTotal' ), 10, 2 );
-		//add_filter( 'wlr_purchase_spent_order_total', array( self::$site, 'handleConditionOrderTotal' ), 10, 2 );
-
-	}
+    function init()
+    {
+        self::$site = empty(self::$site) ? new Main() : self::$site;
+        //self::$admin = empty(self::$admin) ? new Main() : self::$admin;
+        add_filter('wlr_core_multicurrency_allowed', function ($status) {
+            return false;
+        });
+        if (is_admin()) {
+            add_action('admin_menu', array(self::$site, 'addMenu'));
+            add_action('admin_footer', array(self::$site, 'menuHide'));
+        }
+        add_filter('wlr_default_product_price', array(self::$site, 'getDefaultProductPrice'), 10, 5);
+        add_filter('wlr_product_price', array(self::$site, 'getProductPrice'), 10, 4);
+        /*add_filter('wlr_convert_to_default_currency', array(self::$site, 'convertToDefaultCurrency'), 10, 2);*/
+        /*Woocommerce Helper Functions*/
+        add_filter('wlr_current_currency', array(self::$site, 'getCurrentCurrencyCode'));
+        //Purchase Last order amount
+        add_filter('wlr_get_order_total', array(self::$site, 'handleWoocommerceHelperOrderTotal'), 10, 2);
+        /* Conditions*/
+        /*Subtotal Conditions*/
+        add_filter('wlr_get_cart_subtotal', array(self::$site, 'getCartSubtotal'), 10, 2);
+        add_filter('wlr_get_order_subtotal', array(self::$site, 'getOrderSubtotal'), 10, 2);
+        /*Life time sale value*/
+        //add_filter( 'wlr_life_time_sale_value_order_total', array( self::$site, 'handleConditionOrderTotal' ), 10, 2 );
+        //add_filter( 'wlr_purchase_spent_order_total', array( self::$site, 'handleConditionOrderTotal' ), 10, 2 );
+        /*Custom price change*/
+        add_filter('wlr_custom_default_currency', array(self::$site, 'getDefaultCurrency'), 10);
+        /*add_filter('wlr_custom_price_convert', array(self::$site, 'convertToDefaultCurrency'), 10, 2);*/
+    }
 }
